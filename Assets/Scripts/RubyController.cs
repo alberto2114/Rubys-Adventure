2b02d127ead315c8d7402fb8a09bc61cf3d6e012
alberto2114 <90263353+alberto2114@users.noreply.github.com>
2021-09-28 15:17:53 +0200

    check script

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -18,8 +18,8 @@ public class RubyController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
 
         Vector2 position = transform.position;
-        position.x = position.x + 0.5f * horizontal * Time.deltaTime;
-        position.y = position.y + 0.5f * vertical * Time.deltaTime;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
